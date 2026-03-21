2228ae8a10ba8e22d53045f78c763439fc32b894
Davi Dantas <davidantasdev@gmail.com>
2026-03-21 09:33:27 -0300

    feat(auth): adicionar model User e configuração inicial do DRF

---
+++ moneta_backend/moneta/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    cpf = models.CharField(max_length=11, unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
